d38d319fbd2737304e895fa76199636a8304b344
aayyus <91897477+aayyus@users.noreply.github.com>
2024-03-19 20:07:44 +0545

    cursor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,87 @@
-import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import React, { useEffect,useState } from 'react';
+import {motion}  from 'framer-motion';
+
+
 import "../styles/Home.css"
+
+
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
 function Home() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const updateMousePosition = debounce((e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  }, 10);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "crimson",
+      mixBlendMode: 'difference'
+    }
+  }
+
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
+
   return (
+    
     <div className="home">
+    
+    
       <div className="about">
-        <h3> Hi, My name is Aayush.</h3>
+      <motion.div
+    className='cursor'
+    variants={variants}
+    animate= {cursorVariant}
+    />
+        <h3 onMouseEnter={textEnter} onMouseLeave={textLeave}> Hi, My name is Aayush.</h3>
         <div className="prompt"> <p> A web developer with a passion for learning and creating.</p>
-        {/* <LinkedInIcon/>
-        <FacebookIcon/>
-        <InstagramIcon/> */}
+          {/* <LinkedInIcon/>
+<FacebookIcon/>
+<InstagramIcon/> */}
         </div>
       </div>
       <div className="skills">
-         <h1> Skills</h1>
-         <ol className="list">
+        <h1> Skills</h1>
+        <ol className="list">
           <li className="item">
             <h2> Front-End</h2>
             <span>HTML, CSS, JS, React, Next Js, minimals, Angular</span>
-            </li> 
-            <li className="item">
+          </li>
+          <li className="item">
             <h2> Languages</h2>
             <span>C, C++, Python, firebase, wordpress</span>
-            </li> 
+          </li>
 
-         </ol>
-        </div>
+        </ol>
+      </div>
     </div>
   );
 }
