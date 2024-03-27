a2f6d943a0fab376c3456a42d6a731ebd1df7019
aayyus <91897477+aayyus@users.noreply.github.com>
2024-03-27 19:14:12 +0545

    cursor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,63 +1,67 @@
-import React from 'react';
-// import {motion}  from 'framer-motion';
+import React,{useState, useEffect} from 'react';
+import {motion}  from 'framer-motion';
 
 
 import "../styles/Home.css"
 
 
-// function debounce(func, wait) {
-//   let timeout;
-//   return function executedFunction(...args) {
-//     const later = () => {
-//       clearTimeout(timeout);
-//       func(...args);
-//     };
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//   };
-// }
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
 
 function Home() {
-  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  // const [cursorVariant, setCursorVariant] = useState("default");
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [cursorVariant, setCursorVariant] = useState("default");
 
-  // const updateMousePosition = debounce((e) => {
-  //   setMousePosition({ x: e.clientX, y: e.clientY });
-  // }, 10);
+  const updateMousePosition = debounce((e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  }, 10);
 
-  // useEffect(() => {
-  //   window.addEventListener("mousemove", updateMousePosition);
-  //   return () => {
-  //     window.removeEventListener("mousemove", updateMousePosition);
-  //   };
-  // }, []);
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
 
-  // const variants = {
-  //   default: {
-  //     x: mousePosition.x - 16,
-  //     y: mousePosition.y - 16,
-  //   },
-  //   text: {
-  //     height: 150,
-  //     width: 150,
-  //     x: mousePosition.x - 75,
-  //     y: mousePosition.y - 75,
-  //     backgroundColor: "crimson",
-  //     mixBlendMode: 'difference'
-  //   }
-  // }
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: 'difference'
+    }
+  }
 
-  // const textEnter = () => setCursorVariant("text");
-  // const textLeave = () => setCursorVariant("default");
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
 
   return (
     
     <div className="home">
     
-    
+    <motion.div
+          className='cursor'
+          variants={variants}
+          animate={cursorVariant}
+        />
+
       <div className="about">
-    
-        <h3 > Hi, My name is Aayush.</h3>
+        <h3 className='name' onMouseEnter={textEnter} onMouseLeave={textLeave}> Hi, My name is Aayush.</h3>
         <div className="prompt"> <p> A web developer with a passion for learning and creating.</p>
           {/* <LinkedInIcon/>
 <FacebookIcon/>
